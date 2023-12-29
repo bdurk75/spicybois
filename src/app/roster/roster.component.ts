21347d1a39fee069b2520ff188704580469edbf2
Brian Durkin Jr <tui80358@temple.edu>
2023-12-28 20:32:11 -0500

    New Roster Update
    New roster update

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -2,17 +2,31 @@ import { Component } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 
 export interface SpicyBoisRoster {
+  number: string;
   name: string;
   position: string;
-  height: string;
-  weight: number;
 }
 
 const SPICYBOIS_DATA: SpicyBoisRoster[] = [
-  {position: 'ST', name: 'Matt Murphy', height: "6'11\"", weight: 4},
-  {position: 'CB', name: 'Andrew McLaughlin', height: "5'5\"", weight: 400},
-  {position: 'CB', name: 'Brendan Gallagher', height: "5'6\"", weight: 400},
-  {position: 'Bench', name: 'Brian Durkin Jr.', height: "7'0\"", weight: 120}
+  {number:"6", position: 'ST', name: 'Matt Murphy'},
+  {number:"5", position: 'CB', name: 'Andrew McLaughlin'},
+  {number:"69", position: 'CB', name: 'Brendan Gallagher'},
+  {number:"75", position: 'Bench', name: 'Brian Durkin Jr.'},
+  {number:"10", position: 'CM', name: 'Felipe Garces'},
+  {number:"11", position: 'CAM', name: 'Joshua Deets'},
+  {number:"12", position: 'CB', name: 'Kevin Murphy'},
+  {number:"13", position: 'GK', name: 'Jo Kehinde'},
+  {number:"", position: 'CM', name: 'Zach Yazdani'},
+  {number:"21", position: 'CAM', name: 'Mike Gaijan'},
+  {number:"22", position: 'RB', name: 'Brendan McLaughlin'},
+  {number:"23", position: 'CM', name: 'Jake Guevara'},
+  {number:"", position: 'CDM', name: 'Zach Hartzog'},
+  {number:"27", position: 'FW', name: 'Matt Farrell'},
+  {number:"32", position: 'GK', name: 'Ben Krongard'},
+  {number:"33", position: 'LB', name: 'Tommaso Girelli'},
+  {number:"52", position: 'ST', name: 'Vincent Carosella'},
+  {number:"65", position: 'FW', name: 'Jimmy Perrin'},
+  {number:"99", position: 'CM', name: 'Temi Olaoluniyu'}
 ];
 
 @Component({
@@ -22,6 +36,6 @@ const SPICYBOIS_DATA: SpicyBoisRoster[] = [
 })
 
 export class RosterComponent {
-  displayedColumns: string[] = ['position', 'name', 'height', 'weight'];
+  displayedColumns: string[] = ['number','position', 'name'];
   dataSource = SPICYBOIS_DATA;
 }
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,20 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+export interface SpicyBoisRoster {
+  date: string;
+  time: string;
+  opponent: string;
+  location: string;
+}
+
+const SPICYBOIS_DATA: SpicyBoisRoster[] = [
+  {date: '1/1', time: '9:00 a.m.', opponent: "Real Madrid CF", location: "Anders Road"},
+  {date: '1/8', time: '1:00 p.m.', opponent: "Manchester United FC", location: "Anders Road"},
+  {date: '1/15', time: '10:30 a.m.', opponent: "FC Barcelona", location: "Camp Nou"},
+  {date: '1/22', time: '4:00 p.m.', opponent: "Liverpool FC", location: "Anders Road"},
+  {date: '1/45', time: '11:00 a.m.', opponent: "FC Bayern Munich", location: "Anders Road"}
+];
 
 @Component({
   selector: 'app-schedule',
@@ -6,5 +22,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent {
-
+  displayedColumns: string[] = ['date', 'time', 'opponent', 'location'];
+  dataSource = SPICYBOIS_DATA;
 }
