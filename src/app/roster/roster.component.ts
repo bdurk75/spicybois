7b738db4259b47bec60e951b91c3e471ce4b082d
Brian Durkin Jr. <tui80358@temple.edu>
2024-09-25 18:02:31 -0400

    Roster Update

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -10,15 +10,12 @@ export interface SpicyBoisRoster {
 const SPICYBOIS_DATA: SpicyBoisRoster[] = [
   {number:"00", position: 'RM', name: 'Cephas Toninga'},
   {number:"1", position: 'FW', name: 'Nicholas Roback'},
-  {number:"2", position: 'FW', name: 'Isaiah Fox'},
   {number:"3", position: 'CB', name: 'Jon Richards'},
   {number:"4", position: 'CB', name: 'Noah Kwortnik'},
   {number:"5", position: 'CB', name: 'Andrew McLaughlin'},
-  {number:"6", position: 'CM', name: 'Zach Yazdani'},
   {number:"7", position: 'FW', name: 'Jimmy Perrin'},
   {number:"8", position: 'CDM', name: 'Zach Hartzog'},
   {number:"9", position: 'ST', name: 'Matt Murphy'},
-  {number:"10", position: 'CM', name: 'Felipe Garces'},
   {number:"11", position: 'CAM', name: 'Joshua Deets'},
   {number:"12", position: 'CB', name: 'Kevin Murphy'},
   {number:"13", position: 'GK', name: 'Jo Kehinde'},
@@ -27,10 +24,8 @@ const SPICYBOIS_DATA: SpicyBoisRoster[] = [
   {number:"19", position: 'CAM', name: 'Mike Gajian'},
   {number:"22", position: 'RB', name: 'Brendan McLaughlin'},
   {number:"23", position: 'CM', name: 'Jake Guevara'},
-  {number:"24", position: 'ST', name: 'Enrico Romano'},
-  {number:"25", position: 'LM/LB', name: 'Moussa Kanate'},
+  {number:"25", position: 'LB/RB', name: 'Billy Wang'},
   {number:"27", position: 'FW', name: 'Matt Farrell'},
-  {number:"30", position: 'FW', name: 'Brandon Ortiz'},
   {number:"31", position: 'FB', name: 'Jose Ortiz'},
   {number:"32", position: 'GK', name: 'Ben Krongard'},
   {number:"33", position: 'LB', name: 'Tommaso Girelli'},
@@ -38,7 +33,10 @@ const SPICYBOIS_DATA: SpicyBoisRoster[] = [
   {number:"52", position: 'ST', name: 'Vincent Carosella'},
   {number:"66", position: 'CDM', name: 'Joel Mbock'},
   {number:"75", position: 'Right Bench', name: 'Brian Durkin Jr.'},
-  {number:"99", position: 'CM', name: 'Temi Olaoluniyi'}
+  {number:"99", position: 'CM', name: 'Temi Olaoluniyi'},
+  {number:"", position: 'ST', name: 'Mike Chaffee'},
+  {number:"", position: 'CAM', name: 'Aidan Jerome'},
+  {number:"", position: 'CM', name: 'Brendan MacKay'}
 ];
 
 @Component({
